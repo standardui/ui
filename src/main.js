b5d5ebcdd12aeacc999264d24e0ebf942e4026c4
davidcraig <davidcraig1986@gmail.com>
2021-11-24 11:22:30 +0000

    Create methods of registering components with a given namespace.

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import CurrencyInput from './currency-input/currency-input.js'
+import IntegerInput from './integer-input/integer-input.js'
+
+export const COMPONENTS = {
+  CURRENCY_INPUT: {
+    name: 'currency-input',
+    component: CurrencyInput
+  },
+  INTEGER_INPUT: {
+    name: 'integer-input',
+    component: IntegerInput
+  }
+}
+
+const components = [
+  COMPONENTS.CURRENCY_INPUT,
+  COMPONENTS.INTEGER_INPUT
+]
+
+/*
+ * Registers all components in the UI (probably not recommended).
+ */
+export const registerUi = (namespace) => {
+  console.debug(`StandardUI: registering UI for namespace: ${namespace}`)
+  components.forEach(component => {
+    const componentName = `${namespace}-${component.name}`
+    console.debug(`StandardUI: registering ${componentName}`)
+    if (!window.customElements.get(componentName)) {
+      window.customElements.define(componentName, component.component)
+    }
+  })
+}
+
+export default registerUi
