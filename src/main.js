d86c611c1838c77f2a35873bdff5698bc3293deb
davidcraig <davidcraig1986@gmail.com>
2021-11-24 15:16:31 +0000

    Add ability to register custom list of components.
    switch to promise based imports.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
-// import CurrencyInput from './currency-input/currency-input.js'
-import IntegerInput from './integer-input/integer-input.js'
-
 export const COMPONENTS = {
   // CURRENCY_INPUT: {
   //   name: 'currency-input',
-  //   component: CurrencyInput
+  //   path: './currency-input/currency-input.js'
   // },
   INTEGER_INPUT: {
     name: 'integer-input',
-    component: IntegerInput
+    path: './integer-input/integer-input.js'
   }
 }
 
@@ -17,16 +14,26 @@ const components = [
   COMPONENTS.INTEGER_INPUT
 ]
 
-/*
+/**
  * Registers all components in the UI (probably not recommended).
+ * @param {*} namespace The namespace to register the components into.
  */
-export const registerUi = (namespace) => {
-  console.debug(`StandardUI: registering UI for namespace: ${namespace}`)
+export const registerUi = (namespace = 'standardui') => {
+  registerComponents(namespace, components)
+}
+
+/**
+ * Register only specific components in the UI.
+ * @param {*} namespace
+ * @param {*} components
+ */
+export const registerComponents = (namespace, components) => {
   components.forEach(component => {
     const componentName = `${namespace}-${component.name}`
-    console.debug(`StandardUI: registering ${componentName}`)
     if (!window.customElements.get(componentName)) {
-      window.customElements.define(componentName, component.component)
+      import(component.path).then(component => {
+        window.customElements.define(componentName, component.default)
+      })
     }
   })
 }
